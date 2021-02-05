576ef29b8acaebceb62771683edb8ac3aeca99fc
DESKTOP-QJ9HE6B\kjkjhk <emanoelvjj>
2021-02-05 08:44:01 -0300

    some thing

--- app/src/main/java/br/com/aluno/usingsomeapis/resource/utils/Constant.kt
+++ app/src/main/java/br/com/aluno/usingsomeapis/resource/utils/Constant.kt
@@ -2,7 +2,7 @@ package br.com.aluno.usingsomeapis.resource.utils
 
 object Constant {
 
-    const val MAIN_URL_GET_ANIMES_LIST = "https://api.jikan.moe/v3/search/anime"
+    const val   MAIN_URL_GET_ANIMES_LIST = "https://api.jikan.moe/v3/search/anime"
 //    const val MAIN_URL_GET_ANIME = "https://api.jikan.moe/v3/search/"
 
     const val MAIN_URL_GET_EPISODES_OF_ANIME = "https://api.jikan.moe/v3/anime/"
